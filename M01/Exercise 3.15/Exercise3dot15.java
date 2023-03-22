09d7c622ab099f7cca4c69e95ff67e746096f58a
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-03-22 16:04:45 -0400

    Halfway mark for 3.15

--- M01/Exercise 3.15/Exercise3dot15.java
+++ M01/Exercise 3.15/Exercise3dot15.java
@@ -1,3 +1,33 @@
+import java.util.Scanner; //Importing Scanner class
 public class Exercise3dot15 {
-        
+        public static void main(String[] args) { //Declaring main method
+            
+
+            Scanner myObj = new Scanner(System.in); //Creating a Scanner object
+            System.out.println("Enter a lucky three digit number(Ex: 123): "); //Prompting the user to enter a lucky three digit number
+            int luckyNumber = myObj.nextInt(); //variable storing the user input
+
+
+        }
+        public static void passLuckyNumber(int luckyNumber ) { //Declaring method name that really is passLuckyNumber
+            int randomLotteryNumber = (int)(Math.random() * 999); //variable storing a random number between 0 and 999
+
+
+            int firstLuckyDigit = luckyNumber % 10; //variable storing the first number
+            int secondLuckyDigit = (luckyNumber / 10) % 10; //variable storing the second number
+            int thirdLuckyDigit = luckyNumber / 100; //variable storing the third number
+
+
+            int firstLotteryDigit = randomLotteryNumber % 10; //variable storing the first lottery number
+            int secondLotteryDigit = (randomLotteryNumber / 10) % 10; //variable storing the second lottery number
+            int thirdLotteryDigit = randomLotteryNumber / 100; //variable storing the third lottery number
+
+            System.out.println("Your lucky number is " + luckyNumber); //outputs the lucky number
+            System.out.println("The lottery number is " + randomLotteryNumber);
+            
+            
+
+
+        }
+
 }
--- M01/ReadMe.md
+++ M01/ReadMe.md
@@ -5,6 +5,7 @@ EXERCISE 1.5 \/\/\/\/\/\/
 9.5 x 4.5 - 2.5 x 3 / 45.5 -3.5
 
 EXERCISE 2.3 \/\/\/\/\/\/
+
 2.3 (CONVERT FEET INTO METERS) Write a program that reads a number in feet, converts it to meters, and displays the result. One foot is 0.305 meter. Here is a sample run:
 
 
@@ -13,3 +14,11 @@ Enter a value for feet: 16.5
 16.5 feet is 5.0325 meters
 
 EXERCISE 3.15 \/\/\/\/\/\/
+
+**3.15 (GAME: LOTTERY) Revise Listing 3.8, Lottery.java, to generate a lottery of a three-digit integer. The program prompts the user to enter a three-digit integer and determines whether the user wins according to the following rules:
+
+If the user input matches the lottery number in the exact order, the award is $10,000.
+
+If all digits in the user input match all digits in the lottery number, the award is $3,000.
+
+If one digit in the user input matches a digit in the lottery number, the award is $1,000.
