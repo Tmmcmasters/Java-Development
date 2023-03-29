6c4c77cb5cb117ed66404d9d3f303a711020c685
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-03-29 18:00:17 -0400

    Exercise 4.1 completed

--- M02/Exercise 4.1/exerciseFourDotOne.java
+++ M02/Exercise 4.1/exerciseFourDotOne.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; //Import the Scanner class
 
 public class exerciseFourDotOne {
     public static void main(String[] args) {
@@ -7,15 +7,19 @@ public class exerciseFourDotOne {
         System.out.println("Enter the length from the center to a vertex: ");
         double length = scanner.nextDouble();
 
-        double pentagonArea = findPentagonArea(length);
+        double pentagonArea = findPentagonArea(length); //calls the method to calculate the area
 
 
         System.out.println("The area of the pentagon is: " + pentagonArea);
         
     }
 
+    //Method that calculates the area of a pentagon
     static double findPentagonArea(double length) {
-        return (5 * Math.pow(length, 2)) / (4 * Math.tan(Math.PI / 5));
+
+        double side = 2 * (length) * Math.sin(Math.PI / 5); //Calculates the side of the pentagon
+
+        return Math.round((5 * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / 5)) * 100.0) / 100.0; //Calcualtes the area using the side and rounds it
     }
 
 
