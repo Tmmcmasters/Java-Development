79d9b47c8dfc1d7927d090df2f5107ddf114d7c1
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-04-04 17:17:41 -0400

    Adding comments to 4.23

--- M02/Exercise 4.23/exercise4dot23.java
+++ M02/Exercise 4.23/exercise4dot23.java
@@ -3,25 +3,32 @@ import java.util.Scanner;
 public class exercise4dot23 {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
+
+            // Taking the employee's name from the user
             System.out.println("Enter the employee's name: ");
             String employeeName = scanner.nextLine();
 
+            // Taking the number of hours worked in a week from the user
             System.out.println("Enter the number of hours worked in a week: ");
             double hoursWorked = scanner.nextDouble();
 
+            // Taking the hourly pay rate from the user
             System.out.println("Enter the hourly pay rate: ");
             double hourlyPayRate = scanner.nextDouble();
 
+            // Taking the federal tax withholding rate from the user
             System.out.println("Enter the federal tax withhold rate(EX: .20): ");
             double federalTaxWithholdRate = scanner.nextDouble();
 
+            // Taking the tax withholding rate from the user
             System.out.println("Enter the state tax withholding rate(EX:0.09): ");
-            double stateTaxWithholdRate = scanner.nextDouble();
+            double stateTaxWithholdRate = scanner.nextDouble(); 
 
             // Declaring variables
 
-            double grossPay = (hoursWorked * hourlyPayRate);
+            double grossPay = (hoursWorked * hourlyPayRate); // Calculating the gross pay
 
+            // Calculating the deductions and printing everything. There is defintely a better way to do this, but I need to make sure that I have time for other work today. 
             System.out.println("\nEmployee's Name: " + employeeName);
             System.out.println("Hours Worked: " + hoursWorked);
             System.out.println("Hourly Pay Rate: " + hourlyPayRate);
