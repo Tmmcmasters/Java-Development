a8c3cf7fb325915f19c6183c75af0d64311ea2e8
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-04-05 16:22:07 -0400

    Creating files and ReadMe.

---
+++ M03/Exercise 6.9/exercise6dot9.java
@@ -0,0 +1,21 @@
+public class exercise6dot9 {
+
+    // Main method
+    public static void main(String[] args) {
+        
+    }
+
+    // Foot to meter method
+    public static double footToMeter(double foot) {
+        
+        return foot * 0.305;
+        
+    }
+
+    // Meter to foot method
+    public static double meterToFoot(double meter) {
+        
+        return meter * 3.279;
+
+    }
+}
