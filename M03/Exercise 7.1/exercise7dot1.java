8cdb54b8316d047e66aa5366b8901f38525a2a13
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-04-09 00:47:46 -0400

    Finished 7.1

--- M03/Exercise 7.1/exercise7dot1.java
+++ M03/Exercise 7.1/exercise7dot1.java
@@ -9,6 +9,8 @@ public class exercise7dot1 {
 
         int [] arrayOfScores = getScores(students); // Get scores from user
 
+        printScores(arrayOfScores); // Print scores from user
+
     }
 
     public static int[] getScores(int numberOfStudents) {
@@ -21,31 +23,51 @@ public class exercise7dot1 {
         int [] integarArrayOfScores = new int[numberOfStudents]; // Create array of scores
         
        //convert the string scores into an array of integers Ex: "24 52 43" -> [24, 52, 43]
-        for (int i = 0; i < scoresArray.length; i++) {
+        for (int i = 0; i < stringScoresArray.length; i++) {
             integarArrayOfScores[i] = Integer.parseInt(stringScoresArray[i]);
         }
         
-        return arrayOfScores; // Return array of scores
+        return integarArrayOfScores; // Return array of scores
     }
 
     public static void printScores(int [] arrayOfScores) {
         
+        //Print the scores
         for (int counter = 0; counter < arrayOfScores.length; counter++) {
-            System.out.println("Student " + counter + " score is " + arrayOfScores[counter] + " and grade is " + getGrade(arrayOfScores[counter]));
+            System.out.println("Student " + counter + " score is " + arrayOfScores[counter] + " and grade is " + getGrade(arrayOfScores[counter], arrayOfScores)); //Print scores using the getGrade method
         }
     }
 
     public static String getGrade(int score, int [] arrayOfScores)  {
         
+        int highestGrade = getBestGrade(arrayOfScores); // Find the highest score in the array of scores
 
-        
+        // Finds the grade based on the score and the highest score in the array of scores
+        if (score >= highestGrade - 10) {
+            return "A";
+        } else if (score >= highestGrade - 20) {
+            return "B";
+        } else if (score >= highestGrade - 30) {
+            return "C";
+        } else if (score >= highestGrade - 40) {
+            return "D";
+        } else {
+            return "F";
+        }
+       
     }
 
-    public static String bestGrade ( int [] arrayOfScores) {
+    public static int getBestGrade ( int [] arrayOfScores) {
         
         //Find the highest integar inn the array of scores
-        
+        int highestScore = arrayOfScores[0];
+        for (int counter = 0; counter < arrayOfScores.length; counter++) {
+            if (arrayOfScores[counter] > highestScore) {
+                highestScore = arrayOfScores[counter];
+            }
+        }
 
+        return highestScore;
     }
 
     
