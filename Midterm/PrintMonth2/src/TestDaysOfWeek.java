78fd90256fdbd15877231fccfabab08f4d882dc1
Timothy McMasters <97980473+Tmmcmasters@users.noreply.github.com>
2023-04-15 09:51:34 -0400

    Updated for Midterm Part 2

---
+++ Midterm/PrintMonth2/src/TestDaysOfWeek.java
@@ -0,0 +1,11 @@
+import daysAndDates.DaysOfWeek;
+
+public class TestDaysOfWeek {
+    public static void main(String[] args) {
+        System.out.println("Days Of week: ");
+
+        for (int i = 1; i <= 7; i++) {
+            System.out.println("Number: " + i + "\tDay Of Week: " + DaysOfWeek.DayOfWeekStr(i) );
+        }
+    }
+}
